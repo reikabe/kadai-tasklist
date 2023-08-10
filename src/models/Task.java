d455815027e271bc94617310ff0f5687b798ecae
reikabe <kr.kamde@icloud.com>
2023-08-11 08:42:01 +0900

    ShowServlet.javaを作成

--- src/models/Task.java
+++ src/models/Task.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
             query = "SELECT m FROM Task AS m ORDER BY m.id DESC")
 })
 @Table(name = "tasks")
-public class Task {
+public class Task{
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
