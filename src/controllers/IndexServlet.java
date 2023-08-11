f68d90cf84587785063f5d5b7e6991c4ad934505
reikabe <kr.kamde@icloud.com>
2023-08-11 17:10:06 +0900

    Revert "Revert "AlltasklistをAllTaskリストに修正""
    This reverts commit 50077a7485290f83b832d70e54331ea90c9eda2d.

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,7 +35,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task>tasks = em.createNamedQuery("getAlltasks",Task.class).getResultList();
+        List<Task>tasks = em.createNamedQuery("getAllTasks",Task.class).getResultList();
         response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
